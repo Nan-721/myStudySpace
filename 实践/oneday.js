cbee859839118a62a6e16f5fb082ced50bb8e6da
nan <2315014598@qq.com>
2023-02-25 17:14:35 +0800

    test

--- 实践/oneday.js
+++ 实践/oneday.js
@@ -1,1 +1,27 @@
-// 箭头函数和function
+// 变量作用域 （尽可能避免使用全局变量）
+var myVariable = 'global';
+var myOtherVariable = 'global';
+function myFunction() {
+    var myVariable = 'local';
+    console.log(this);
+    return myVariable;
+}
+function myOtherFunction() {
+    myOtherVariable = 'local';
+
+    return myOtherVariable;
+}
+console.log(myVariable); // global
+console.log(myFunction()); // local
+console.log(myOtherVariable); // global
+console.log(myOtherFunction()); // local
+console.log(myOtherVariable); // local
+
+// 位运算符
+
+console.log('5 & 1:',(5 & 1));
+console.log('5 | 1:',(5 | 1));
+console.log('~ 1:',(~ 1));
+console.log('5 ^ 1 :>> ', 5 ^ 1);
+console.log('5 z :>> ', 5 z);
+
